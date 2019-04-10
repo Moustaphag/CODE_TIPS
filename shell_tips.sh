dcdec35e847c9dd16c85bf895185f7dd2e2f54cb
Moustapha Gaoh <gaohmoustapha@gmail.com>
2019-04-10 18:04:02 +0200

    Update shell_tips.sh

--- shell_tips.sh
+++ shell_tips.sh
@@ -6,3 +6,9 @@ git commit -m "NUMERO_TICKET: BLABLABLA"
 git push origin HEAD
 
 ## 2
+git branch
+git checkout -b nom_de_la_nouvelle_branche 
+git status
+git add .
+git commit -m "NUMERO_TICKET: BLABLABLA"
+git push origin HEAD
